83a1c54a716c61893c561046764a297a9f1d72ed
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-07-22 18:01:31 -0300

    finished 🎉

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useReducer, useState } from "react";
+import { differenceInSeconds } from "date-fns";
+import { createContext, ReactNode, useEffect, useReducer, useState } from "react";
 import { ActionTypes, addNewCycleAction, interruptCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
-import {  Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import { Cycle, cyclesReducer } from "../reducers/cycles/reducer";
 
 interface CreateCycleData {
   task: string;
@@ -33,13 +34,37 @@ export function CyclesContextProvider({ children }: CycleProviderProps) {
   const [cyclesState, dispatch] = useReducer(cyclesReducer, {
     cycles: [],
     activeCycleId: null,
-  });
+  }, () => {
+    const cyclesStored = localStorage.getItem("@timer-vite:cycles-state-1.0.0");
+    if (cyclesStored) {
+      return JSON.parse(cyclesStored);
+    }
+    return {
+      cycles: [],
+      activeCycleId: null,
+    }
+  }
+  );
 
   const { activeCycleId, cycles } = cyclesState;
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState<number>(0)
-
-
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState<number>(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+    return 0;
+  })
+
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+    localStorage.setItem("@timer-vite:cycles-state-1.0.0", stateJSON);
+
+
+  }, [cyclesState])
+
 
   const changeSecondsPassed = (seconds: number) => {
     setAmountSecondsPassed(seconds)
--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,65 +1,62 @@
 import { ActionTypes } from "./actions";
-
+import { produce } from "immer";
 
 export interface Cycle {
-    id: string;
-    task: string;
-    minutesAmount: number;
-    startDate: Date;
-    interruptedDate?: Date;
-    finishedDate?: Date;
-  }
-  
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
+  interruptedDate?: Date;
+  finishedDate?: Date;
+}
+
 interface CyclesState {
-    cycles: Cycle[];
-    // activeCycle: Cycle | undefined;
-    activeCycleId: string | null;
-    // amountSecondsPassed: number;
-  
-  }
+  cycles: Cycle[];
+  // activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  // amountSecondsPassed: number;
+
+}
 
 
 
 export function cyclesReducer(state: CyclesState, action: any) {
-    switch (action.type) {
-      case ActionTypes.CREATE_NEW_CYCLE:
-        return {
-          ...state,
-          cycles: [...state.cycles, action.payload.newCycle],
-          activeCycleId: action.payload.newCycle.id
-        }
-      case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return {
-                ...cycle,
-                finishedDate: new Date()
-              }
-            }
-            return cycle
-          }),
-          activeCycleId: null
-        }
+  switch (action.type) {
+    case ActionTypes.CREATE_NEW_CYCLE:
 
-      case ActionTypes.INTERRUPT_CYCLE:
-        return {
-          ...state,
-          cycles: state.cycles.map((cycle) => {
-            if (cycle.id === state.activeCycleId) {
-              return {
-                ...cycle,
-                interruptedDate: new Date()
-              }
-            } else {
-              return cycle;
+      return produce(state, draft => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
 
-            }
-          }),
-          activeCycleId: null
-        }
-      default:
-        return state
-    }
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+
+      const currentFinishedCycleIndex = state.cycles.findIndex(cycle => cycle.id === state.activeCycleId);
+
+      if (currentFinishedCycleIndex < 0) {
+        return state;
+      }
+      else {
+        return produce(state, draft => {
+          draft.cycles[currentFinishedCycleIndex].finishedDate = new Date();
+          draft.activeCycleId = null;
+        })
+      }
+
+    case ActionTypes.INTERRUPT_CYCLE:
+      const currentInterruptCycleIndex = state.cycles.findIndex(cycle => cycle.id === state.activeCycleId);
+
+      if (currentInterruptCycleIndex < 0) {
+        return state;
+      }
+      else {
+        return produce(state, draft => {
+          draft.cycles[currentInterruptCycleIndex].interruptedDate = new Date();
+          draft.activeCycleId = null;
+        })
+      }
+
+    default:
+      return state
   }
+}
